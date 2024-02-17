16db65f2fa6af19a87c781ebe05a079f576dba0a
ing. M.A.C.M. (Martijn) van den Boom <martijn.vandenboom@icloud.com>
2024-02-17 17:26:44 +0100

    added index.html
    added index.html

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-# Importeer de benodigde modules
 from flask import Flask, render_template
 
-# Maak een Flask-applicatie
 app = Flask(__name__)
 
-# Definieer een route voor de startpagina
 @app.route('/')
 def home():
-    return 'Welkom op de startpagina van de Flask-webapplicatie!'
+    # Render de hoofdpagina met een link naar pagina.html
+    return render_template('index.html')
 
-# Definieer een route voor een eenvoudige HTML-pagina
 @app.route('/pagina')
 def pagina():
+    # Render de pagina.html
     return render_template('pagina.html')
 
-# Als dit bestand rechtstreeks wordt uitgevoerd, start dan de app
 if __name__ == '__main__':
     app.run(debug=True)
