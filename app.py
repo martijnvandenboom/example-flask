385f22dd514dc592854802b0bd99d054fdd501a2
ing. M.A.C.M. (Martijn) van den Boom <martijn.vandenboom@icloud.com>
2024-02-17 17:19:25 +0100

    app aangepast naar 1e link
    app aangepast naar 1e link

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+# Importeer de benodigde modules
+from flask import Flask, render_template
+
+# Maak een Flask-applicatie
 app = Flask(__name__)
 
+# Definieer een route voor de startpagina
 @app.route('/')
-def hello_world():
-    return 'Hello World! My name is Martijn van den Boom and I come from the Netherlands.'
+def home():
+    return 'Welkom op de startpagina van de Flask-webapplicatie!'
 
+# Definieer een route voor een eenvoudige HTML-pagina
+@app.route('/pagina')
+def pagina():
+    return render_template('pagina.html')
 
-if __name__ == "__main__":
-    app.run()
+# Als dit bestand rechtstreeks wordt uitgevoerd, start dan de app
+if __name__ == '__main__':
+    app.run(debug=True)
